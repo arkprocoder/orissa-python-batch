5c52fae0dfd37d8032b79a19476301405a7c91ee
anees <aneesurrehman423@gmail.com>
2024-02-19 19:09:16 +0530

    day-1

---
+++ datatypes.py
@@ -0,0 +1,52 @@
+# varaibles : store values
+fname="Naina"
+lname='kumari'
+age=20
+phone=999999999
+# print(fname)
+# print(lname)
+# print(age,phone)
+print(fname,lname,age,phone)
+
+print(type(fname))
+print(type(lname))
+print(type(age))
+print(type(phone))
+
+
+# string
+# int
+
+# boolean : True | False
+isActivate=True
+isActivate=False
+fname="Anees"
+lname="Khan"
+print(isActivate)
+print(fname,lname,age,phone)
+
+# list :mutable
+skills=['Java','Python','Javascript',2,7,6,True,[12,12,13,14]]
+print(skills)
+print(type(skills))
+
+# set
+myset={1,2,3,4,5,1,2,3,4,5}
+print(type(myset))
+print(myset)
+
+# dictionaries
+mydetails={
+    "name":"Anees",
+    "company":"Infosys",
+    "skills":['python','react'],
+    "phone":88888888,
+    "isactive":True
+}
+print(mydetails)
+print(type(mydetails))
+
+# tuple : immutable
+mytup=(1,2,3,4)
+print(mytup)
+print(type(mytup))
---
+++ hello.py
@@ -0,0 +1,21 @@
+print("Hello World!!")
+# print('guvi') single line comment
+
+
+# print('''orissa
+      
+#       hello world
+#       ''')
+
+
+# multi lines comments
+'''
+print("india")
+print("india")
+print("india")
+print("india")
+print("india")
+print("india")
+'''
+
+print("End")
